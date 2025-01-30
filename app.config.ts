6b10e51f18b3e5e243d49a08a34c1d660f115336
Daniil Lobanov <lobanov006@gmail.com>
2025-01-30 14:52:03 +0300

    Configure Expo Updates and EAS project settings

--- app.config.ts
+++ app.config.ts
@@ -4,7 +4,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: 'ЯГТУ ID',
   slug: 'ystu-id',
-  version: '1.0.0',
+  version: '1.0.1',
   orientation: 'portrait',
   icon: './assets/icon.png',
   userInterfaceStyle: 'automatic',
@@ -30,5 +30,8 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     API_URL: process.env.API_URL,
     OAUTH_CLIENT_ID: process.env.OAUTH_CLIENT_ID,
     OAUTH_SCOPE: process.env.OAUTH_SCOPE,
+    eas: {
+        projectId: "2c23029f-1ccc-4c9a-aaa8-3ef8c7c83d32"
+      }
   },
 }); 
